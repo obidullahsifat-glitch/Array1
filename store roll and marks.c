12d929114b2ba6e2f6215f790f1f35fefc28b098
obidullahsifat-glitch <obidullahsifat29210@gmail.com>
2026-09-19 23:36:12 +0600

    Add files via upload

---
+++ store roll and marks.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+
+  int i,j;
+ int arr[4][2];
+  for(i=0;i<=3;i++)
+  {
+      for(j=0;j<=1;j++)
+      {
+
+           scanf("%d",&arr[i][j]);
+
+      }
+  }
+
+
+
+    for(i=0;i<=3;i++)
+    {
+
+
+      for(j=0;j<=1;j++)
+      {
+          printf("%d ",arr[i][j]);
+      }
+      printf("\n");
+  }
+
+
+
+    return 0;
+}
